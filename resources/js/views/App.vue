ef6d979206ef7a3de30dd53c5c9054fe3fbb0bd2
Mattia Cigno <geom.mattiacigno@gmail.com>
2022-06-21 15:30:58 +0200

    created the file home in guest-final commit 21/06/2022

---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    
+    <div>
+        <h1>Work in Progres</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt similique quis fugit consequuntur deserunt voluptas magnam voluptate iste, animi voluptates.</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {}
+}
+</script>
